9729d37577d3b25657432bc0c6abaccebbbd6c0d
PaulMargueritteOclockStudent <paul.margueritte@icloud.com>
2023-08-20 12:13:05 +0200

    centrage sur paris

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([48.856614, 2.3522219], 13);
 
 var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
 	maxZoom: 20,
